c8fecbc262cb497648660700ed50a127423d93f6
akshay22bv <akshay22bv@gmail.com>
2023-08-14 15:40:40 +0530

    first commit

--- src/Content.js
+++ src/Content.js
@@ -11,7 +11,7 @@ import services_logo1 from "./assets/images/Services/logo1.png";
 import services_logo2 from "./assets/images/Services/logo2.png";
 import services_logo3 from "./assets/images/Services/logo3.png";
 
-import project1 from "./assets/images/projects/img1.png";
+import project1 from "./assets/images/projects/shoes.png";
 import project2 from "./assets/images/projects/img2.png";
 import project3 from "./assets/images/projects/portfolio.png";
 import project4 from "./assets/images/projects/dress-ecommerce.png";
